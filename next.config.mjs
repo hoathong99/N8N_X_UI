612a08b78a60b5248944a53299dee8286869d4f2
Vu Ngoc Anh <hoathong99@gmail.com>
2025-06-26 23:37:12 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // ✅ Remove 'output'
   basePath: '/N8N_X_UI',
   assetPrefix: '/N8N_X_UI/',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
 }
-
 export default nextConfig
