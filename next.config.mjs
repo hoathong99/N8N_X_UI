a2e261b0c4c8c68d3339152392b0d834b21efe35
Vu Ngoc Anh <hoathong99@gmail.com>
2025-06-26 23:27:23 +0700

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,11 @@
-// /** @type {import('next').NextConfig} */
-
-// const nextConfig = {
-//   output: 'export', // ✅ tells Next.js to export to static files in /out
-//   basePath: '/N8N_X_UI',
-//   assetPrefix: '/N8N_X_UI/',
-
-//   eslint: { ignoreDuringBuilds: true },
-//   typescript: { ignoreBuildErrors: true },
-//   images: { unoptimized: true },
-// }
-
-// export default nextConfig
-
-// // N8N_X_UI
-
-import { withNetlify } from '@netlify/next'
-
-export default withNetlify({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   basePath: '/N8N_X_UI',
   assetPrefix: '/N8N_X_UI/',
-
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
-})
+}
+
+export default nextConfig
