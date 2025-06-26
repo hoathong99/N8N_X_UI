d8cf79e6e6b7cd7c46810d98a5262586dff2fecb
Vu Ngoc Anh <hoathong99@gmail.com>
2025-06-26 23:22:35 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // ✅ tells Next.js to export to static files in /out
+// /** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+//   output: 'export', // ✅ tells Next.js to export to static files in /out
+//   basePath: '/N8N_X_UI',
+//   assetPrefix: '/N8N_X_UI/',
+
+//   eslint: { ignoreDuringBuilds: true },
+//   typescript: { ignoreBuildErrors: true },
+//   images: { unoptimized: true },
+// }
+
+// export default nextConfig
+
+// // N8N_X_UI
+
+import { withNetlify } from '@netlify/next'
+
+export default withNetlify({
+  reactStrictMode: true,
   basePath: '/N8N_X_UI',
   assetPrefix: '/N8N_X_UI/',
 
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
-}
-
-export default nextConfig
-
-// N8N_X_UI
+})
